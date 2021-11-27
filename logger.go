ceba99335ad234cb876257a34448aa2fd31a2e32
ByBullet <createbybullet@163.com>
2021-11-27 22:37:11 +0800

    修改传参

--- logger.go
+++ logger.go
@@ -9,26 +9,26 @@ var err = log.New(os.Stdout, "error", log.LstdFlags|log.Lshortfile)
 var info = log.New(os.Stdout, "info", log.LstdFlags|log.Lshortfile)
 var debug = log.New(os.Stdout, "debug", log.LstdFlags|log.Lshortfile)
 
-func Error(s interface{}, v ...interface{}) {
-	err.Println(s, v)
+func Error(v ...interface{}) {
+	err.Println(v...)
 }
 
-func Info(s interface{}, v ...interface{}) {
-	info.Println(s, v)
+func Info(v ...interface{}) {
+	info.Println(v...)
 }
 
-func DebugFatal(s interface{}, v ...interface{}) {
-	debug.Fatal(s, v)
+func DebugFatal(v ...interface{}) {
+	debug.Fatal(v...)
 }
 
-func ErrorFatal(s interface{}, v ...interface{}) {
-	err.Fatal(s, v)
+func ErrorFatal(v ...interface{}) {
+	err.Fatal(v...)
 }
 
-func InfoFatal(s interface{}, v ...interface{}) {
-	info.Fatal(s, v)
+func InfoFatal(v ...interface{}) {
+	info.Fatal(v...)
 }
 
-func Debug(s interface{}, v ...interface{}) {
-	debug.Println(s, v)
+func Debug(v ...interface{}) {
+	debug.Println(v...)
 }
---
+++ logger_test.go
@@ -0,0 +1,7 @@
+package logger
+
+import "testing"
+
+func Test_Info(t *testing.T) {
+	Info("1", "2", "3")
+}
